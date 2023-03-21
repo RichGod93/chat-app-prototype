62464679db6babce8041a3557b9ec8a0d06ea89a
RichGod <usenrichgod93@gmail.com>
2023-03-21 15:30:20 +0100

    signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+    return (
+        <>
+            <div>
+                <p>signup</p>
+            </div>
+        </>
+    );
+};
+
+export default SignUp;
