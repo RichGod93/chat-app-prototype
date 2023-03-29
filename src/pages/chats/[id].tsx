76b3cac6e4aaf1d6e525ebeddf77d676d0aa9694
RichGod <usenrichgod93@gmail.com>
2023-03-29 15:17:59 +0100

    .

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -1,7 +1,19 @@
-export default function Chat() {
+import Layout from "@/components/Layout";
+
+export function Chat() {
     return (
         <div>
             <h1>Chat</h1>
         </div>
+    );
+}
+
+export default function Chats() {
+    return (
+        <Layout>
+            <div>
+                <h1>Chats</h1>
+            </div>
+        </Layout>
     );
 }
