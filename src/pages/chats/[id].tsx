265175d74b8b04d64f61c35c303929afaa5b73b7
RichGod <usenrichgod93@gmail.com>
2023-03-30 15:59:18 +0100

    in progress

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -1,19 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import Layout from "@/components/Layout";
+import { Box, Button, HStack, Input, VStack } from "@chakra-ui/react";
+import { collection, onSnapshot, orderBy, query, serverTimestamp, Timestamp } from "firebase/firestore";
+import { db } from '@/config/firebase';
 
 export function Chat() {
     return (
-        <div>
-            <h1>Chat</h1>
-        </div>
+        <Layout>
+            <VStack className="flex flex-col h-screen">
+                <Box className="flex-1"></Box>
+                <HStack width={"90%"} pb={5}>
+                    <Input
+                        borderColor={"gray.700"}
+                        placeholder="Enter message"
+                        variant={"filled"}
+                        size={"lg"}
+                    />
+                    <Button colorScheme={"telegram"} size={"md"}>
+                        Send
+                    </Button>
+                </HStack>
+            </VStack>
+        </Layout>
     );
 }
 
-export default function Chats() {
+interface ChatProps {
+    key: string;
+    id: string;
+    sender: string;
+    message: string;
+    timestamp: Function;
+    received?: boolean;
+}
+
+export default function Chats(chatProps: ChatProps) {
+    const [chats, setChats] = useState([]);
+    // useEffect(() => {
+    //     onSnapshot(
+    //         query(collection(db, 'chats'), orderBy('timestamp', 'asc')),
+    //         (snapshot) => {
+    //             setChats(snapshot.docs);
+    //         }
+    //     );
+    // });
+
     return (
-        <Layout>
-            <div>
-                <h1>Chats</h1>
-            </div>
-        </Layout>
+        <div>
+            <Chat
+                key={chatProps.key}
+                id={chatProps.id}
+                sender={chatProps.sender}
+                message={chatProps.message}
+                timestamp={chatProps.timestamp}
+                recieved={chatProps?.received}
+            />
+        </div>
     );
 }
